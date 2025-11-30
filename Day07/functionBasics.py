0443e27d7a20cc9d40fe36088349362136f71f5c
Chanchal Sharma <chachalsharma@Chachals-MacBook-Air.local>
2025-11-30 17:55:20 +0530

    Updating functionBasics.py

---
+++ Day07/functionBasics.py
@@ -0,0 +1,17 @@
+# Function Basic 
+
+def sumFunc() :
+    a = 4
+    b = 9
+    sum = a + b
+    print(sum)
+
+sumFunc()    
+
+# . . . 100 line of code
+
+sumFunc()
+
+# # . . . 1000 line of code
+
+sumFunc()
---
+++ Day07/practice1.py
@@ -0,0 +1,28 @@
+# Write a function named welcome_message() that prints “Welcome to
+# Python Programming!” three times.
+
+def welcome_message():
+    print("Welcome to Python Programming!")
+
+welcome_message()   # function is being called here
+welcome_message()
+welcome_message()
+
+# Define a function inspire() that prints a motivational quote with your
+# name.
+
+def inspire():
+    print('\n"Nobody can help you better than you!" : Chanchal')
+
+inspire()    
+
+# Create a function good_morning() that prints "Good Morning, Saumya!".
+# Call it twice.
+
+def good_morning() :
+    print("Good Morning, Chanchal!")
+
+good_morning()    
+good_morning()
+
+# Why are functions used in programming? Write two advantages...
