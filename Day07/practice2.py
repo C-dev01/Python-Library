2026657281b5dde5631151aa4b9f5a391dd533a5
Chanchal Sharma <chachalsharma@Chachals-MacBook-Air.local>
2025-11-30 19:25:26 +0530

    Added new file in Day07

---
+++ Day07/practice2.py
@@ -0,0 +1,24 @@
+# 1. Write a function display_python() that prints "Python is Fun!".
+# 2. Create a function learn() that prints three Python topics.
+# 3. Explain what happens if you call a function before defining it.
+
+## 1
+def display_python():
+    print("Python is Fun!")
+
+display_python()
+
+## 2
+def learn():
+    print("\nHigh-level and easy to learn: Python is a high-level, user-friendly language with a clean syntax that prioritizes readability, making it easier to write, understand, and debug code.")
+    print("\nVersatile and dynamic: It is a dynamically typed language, meaning you don't need to declare variables explicitly, and it supports multiple programming paradigms like object-oriented and procedural programming."
+)
+    print("\nExtensive libraries and community: Python has a vast standard library and is supported by a large community, which provides extensive resources and pre-built modules for many different applications, from web development to data science and AI. \n")
+
+learn()
+
+## 3
+def test_fun():
+    print("It is test fun.")
+test_fun()  # Yes there is no problem in than! 
+
